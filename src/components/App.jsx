7af7188a14defe5d2a380912cab7aa3ecf4cda03
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-02-03 18:45:56 -0800

    Add Header and App components.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+function App(){
+  return (
+		<div>
+			<Header/>
+			<h3>3a</h3>
+			<h3>Thato and Haley</h3>
+			<p><em>Firebase will not save record!</em></p>
+		</div>
+	);
+}
+
+export default App;
