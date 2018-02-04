6662cc163b4e3ff97cba2613a5a6505e4eea4a8b
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-02-03 18:54:46 -0800

    Add Ticket and TicketList Components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import TicketList from "./TicketList";
+
 function App(){
   return (
 		<div>
 			<Header/>
-			<h3>3a</h3>
-			<h3>Thato and Haley</h3>
-			<p><em>Firebase will not save record!</em></p>
+			<TicketList />
 		</div>
 	);
 }
