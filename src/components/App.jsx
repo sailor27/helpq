ea402adfec07c3bc2c61dc7aca060a844009fc4c
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-02-20 19:53:22 -0800

    Refactor updateElapsedWaitTime logic to map on to ticketlist props. Create and test update time action, and dispatch in udpateElapsedWaitTime function.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,24 +18,28 @@ class App extends React.Component {
       selectedTicket: null
     };
   }
-componentDidMount() {
+	componentDidMount() {
     this.waitTimeUpdateTimer = setInterval(() =>
       this.updateTicketElapsedWaitTime(),
-    60000
+    6000
     );
   }
-
-  updateTicketElapsedWaitTime() {
-    // let newMasterTicketList = Object.assign({}, this.state.masterTicketList);
-    // Object.keys(newMasterTicketList).forEach((ticketId) => {
-    //   newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true);
-    // });
-    // this.setState({masterTicketList: newMasterTicketList});
-  }
-
   componentWillUnmount(){
     clearInterval(this.waitTimeUpdateTimer);
   }
+	updateTicketElapsedWaitTime() {
+		const { dispatch } = this.props;
+			Object.keys(this.props.masterTicketList).map(ticketId => {
+				const ticket = this.props.masterTicketList[ticketId];
+				const newFormattedWaitTime = ticket.timeOpen.fromNow(true);
+			const action = {
+				type: 'UPDATE_TIME',
+				id: ticketId,
+				formattedWaitTime: newFormattedWaitTime
+			};
+			dispatch(action);
+		});
+	}
 
   render(){
     var appStyle = {
