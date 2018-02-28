f7e6d95d77a71737a0bd78c1a15f80c05248333a
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-02-28 15:56:11 -0800

    Initialize firebase in actions index.

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+import constants from './../constants';
+const { firebaseConfig } = constants;
+import Firebase from 'firebase';
+
+firebase.initializeApp(firebaseConfig);
--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+// 👉 entry point 👈🏽
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
